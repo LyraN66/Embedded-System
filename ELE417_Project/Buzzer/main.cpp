796593158178def4051e4817af1c483304fee941
Ethem Buğra Arslan <101515029+LyraN66@users.noreply.github.com>
2023-12-24 21:29:31 +0300

    Update main.cpp

--- ELE417_Project/Buzzer/main.cpp
+++ ELE417_Project/Buzzer/main.cpp
@@ -1,27 +1,36 @@
 #include "mbed.h"
 #include "platform/mbed_thread.h"
- 
+
 PwmOut buzzer(D3);
- 
+DigitalIn flameSensor(D2);
+
+float Nota_C4 = 262;
+float Nota_A4 = 440;
+float Nota_E4 = 659;
+
+void playMelody() {
+    buzzer.period(1.0/Nota_C4);
+    buzzer.write(0.5);
+    thread_sleep_for(200);
+
+    buzzer.period(1.0/Nota_A4);
+    buzzer.write(0.5);
+    thread_sleep_for(200);
+
+    buzzer.period(1.0/Nota_E4);
+    buzzer.write(0.5);
+    thread_sleep_for(200);
+}
+
 int main() {
- 
- float Nota_C4 = 262;
- float Nota_A4 = 440;
- float Nota_E4 = 659;
- 
- while(1){
-   
-   buzzer.period(1.0/Nota_C4); // Configurando el periodo, que es equivalente a frecuencia (veces que se reproducira el tono por segundo)
-   buzzer.write(0.5);
-   thread_sleep_for(200);
-   
-   buzzer.period(1.0/Nota_A4); // Configurando el periodo, que es equivalente a frecuencia (veces que se reproducira el tono por segundo)
-   buzzer.write(0.5);
-   thread_sleep_for(200);
-   
-   buzzer.period(1.0/Nota_E4); // Configurando el periodo, que es equivalente a frecuencia (veces que se reproducira el tono por segundo)
-   buzzer.write(0.5);
-   thread_sleep_for(200);
-   
- }
+    while(1){
+        if(flameSensor.read() == 0) { // Alev algılandı
+            for(int i = 0; i < 5; i++) {
+                playMelody(); // Melodiyi çal
+            }
+        } else {
+            buzzer.write(0); // Alev algılanmadığında buzzer'ı kapat
+        }
+        thread_sleep_for(100); // Kısa bir bekleme süresi
+    }
 }
