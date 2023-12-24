fb63c5646f7e3d765d6ce26e14ae65984a9e3b5c
Ethem Buğra Arslan <101515029+LyraN66@users.noreply.github.com>
2023-12-24 21:49:04 +0300

    Update main.cpp

--- ELE417_Project/Buzzer/main.cpp
+++ ELE417_Project/Buzzer/main.cpp
@@ -4,30 +4,40 @@
 PwmOut buzzer(D3);
 DigitalIn flameSensor(D2);
 
-float Nota_C4 = 262;
-float Nota_A4 = 440;
-float Nota_E4 = 659;
+float AlarmNote1 = 200; 
+float AlarmNote2 = 350; 
+float AlarmNote3 = 250;
+float AlarmNote4 = 400;
+float AlarmNote5 = 150;
 
-void playMelody() {
-    buzzer.period(1.0/Nota_C4);
-    buzzer.write(0.5);
-    thread_sleep_for(200);
+void playFireAlarm() {
+    for(int i = 0; i < 10; i++) {
+        buzzer.period(1.0/AlarmNote1);
+        buzzer.write(0.5);
+        thread_sleep_for(100);
 
-    buzzer.period(1.0/Nota_A4);
-    buzzer.write(0.5);
-    thread_sleep_for(200);
+        buzzer.period(1.0/AlarmNote2);
+        buzzer.write(0.5);
+        thread_sleep_for(100);
 
-    buzzer.period(1.0/Nota_E4);
-    buzzer.write(0.5);
-    thread_sleep_for(200);
+        buzzer.period(1.0/AlarmNote3);
+        buzzer.write(0.5);
+        thread_sleep_for(100);
+
+        buzzer.period(1.0/AlarmNote4);
+        buzzer.write(0.5);
+        thread_sleep_for(100);
+
+        buzzer.period(1.0/AlarmNote5);
+        buzzer.write(0.5);
+        thread_sleep_for(100);
+    }
 }
 
 int main() {
     while(1){
         if(flameSensor.read() == 0) { // Alev algılandı
-            for(int i = 0; i < 5; i++) {
-                playMelody(); // Melodiyi çal
-            }
+            playFireAlarm(); // Yangın alarmı melodisini çal
         } else {
             buzzer.write(0); // Alev algılanmadığında buzzer'ı kapat
         }
